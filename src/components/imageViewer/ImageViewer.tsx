627d8e6b003aa7298488127b94a28ddebe6c74f6
jmjles <jmjles@gmail.com>
2026-02-18 14:15:21 -0800

    devop:minor bug, removed hardcoded img

--- src/components/imageViewer/ImageViewer.tsx
+++ src/components/imageViewer/ImageViewer.tsx
@@ -3,7 +3,6 @@ import { ArrowBack, ArrowForward, Close } from "@mui/icons-material";
 import { Box, Fab, Grid, Modal } from "@mui/material";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import home from "@/assets/landing.jpg";
 const ImageViewer = (props: ImageViewerProps) => {
   const images = [props.featuredImage, ...props.images];
   const [current, setCurrent] = useState(props.selectedImage);
@@ -18,7 +17,7 @@ const ImageViewer = (props: ImageViewerProps) => {
   }, [props.selectedImage]);
 
   const backDisabled = current === 0;
-  const nextDisabled = current === images.length;
+  const nextDisabled = current === images.length - 1;
 
   return (
     <Modal open={props.open} onClose={props.close}>
@@ -29,7 +28,7 @@ const ImageViewer = (props: ImageViewerProps) => {
         sx={{ transform: "translate(-50%,-50%)" }}
       >
         <Box
-        textAlign={"right"}
+          textAlign={"right"}
           position={{ xs: "absolute", md: "initial" }}
           right={{ xs: 22, md: 0 }}
           top={{ xs: 12, md: 0 }}
@@ -53,27 +52,35 @@ const ImageViewer = (props: ImageViewerProps) => {
               left={{ xs: 22, md: 0 }}
               zIndex={99}
             >
-              <Fab onClick={handleBack} disabled={backDisabled} color="info" size="small">
-                <ArrowBack fontSize="small"/>
+              <Fab
+                onClick={handleBack}
+                disabled={backDisabled}
+                color="info"
+                size="small"
+              >
+                <ArrowBack fontSize="small" />
               </Fab>
             </Box>
 
             <Box
               width={{ xs: "100vw", md: "70vw" }}
-              minHeight={"50vw"}
-              maxHeight={{ xs: "100vh", md: "90vh" }}
               position="relative"
               sx={{ aspectRatio: "16/9" }}
             >
-              <Image src={home} alt="asd" fill />
+              <Image src={images[current]} alt="asd" fill />
             </Box>
             <Box
               position={{ xs: "absolute", md: "relative" }}
               right={{ xs: 22, md: 0 }}
               zIndex={99}
             >
-              <Fab onClick={handleNext} disabled={nextDisabled} color="info" size="small">
-                <ArrowForward fontSize="small"/>
+              <Fab
+                onClick={handleNext}
+                disabled={nextDisabled}
+                color="info"
+                size="small"
+              >
+                <ArrowForward fontSize="small" />
               </Fab>
             </Box>
           </Grid>
